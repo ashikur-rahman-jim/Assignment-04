2829927845184bd785e9ff457c7683e0ff48e450
ashikur-rahman-jim <ashikurrahman50660@gmail.com>
2026-02-22 11:39:19 -0500

    Create script file and styled toggle button

---
+++ machine.js
@@ -0,0 +1,5 @@
+// Get ID
+function getId(id) {
+    const idGet = document.getElementById(id);
+    return idGet;
+}
---
+++ script.js
@@ -0,0 +1,39 @@
+// Get Job counts
+const total = getId('total');
+const interview = getId('interview');
+const rejected = getId('rejected');
+const jobs = getId('jobs');
+
+// Get all-cards and all-cards length add to total,jobs.
+const allCards = getId('all-cards');
+total.innerText = allCards.children.length;
+jobs.innerText = allCards.children.length;
+
+// Style toggle button
+const allBtn = getId('all-btn');
+const interviewdBtn = getId('interview-btn');
+const rejectedBtn = getId('rejected-btn');
+
+function toggleBtn(id) {
+    allBtn.classList.remove('btn-primary');
+    interviewdBtn.classList.remove('btn-primary');
+    rejectedBtn.classList.remove('btn-primary');
+
+    allBtn.classList.add('btn-outline');
+    interviewdBtn.classList.add('btn-outline');
+    rejectedBtn.classList.add('btn-outline');
+
+    const select = getId(id);
+    select.classList.remove('btn-outline');
+    select.classList.add('btn-primary');
+
+    if(id === 'interview-btn' || id === 'rejected-btn') {
+        // const cards = document.querySelectorAll('.cards').classList.add('hidden');
+        const allCards = getId('all-cards').classList.add('hidden');
+        const noJobs = getId('no-jobs').classList.remove('hidden');
+    } else {
+        const allCards = getId('all-cards').classList.remove('hidden');
+        const noJobs = getId('no-jobs').classList.add('hidden');
+    }
+    
+}
